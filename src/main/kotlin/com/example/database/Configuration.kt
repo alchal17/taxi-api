97c95581367943c0b1853e41b06957ee2d9f5113
alchal17 <79000644+alchal17@users.noreply.github.com>
2024-05-20 13:11:12 +0300

    Update Configuration.kt

--- src/main/kotlin/com/example/database/Configuration.kt
+++ src/main/kotlin/com/example/database/Configuration.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.Database
 
 fun configureDatabase() {
     Database.connect(
-        url = "jdbc:postgresql://ha.bigline.net:5432/dbpropro2023?user=pzpi219&password=nure.pzpi21-9&timezone=UTC",
+        url = "",
         driver = "org.postgresql.Driver",
     )
 }
